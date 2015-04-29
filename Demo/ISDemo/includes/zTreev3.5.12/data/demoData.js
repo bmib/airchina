c7cda98bb00e93442c57ad46edc9ec0aa71485ed
anatkh <joseph.1@163.com>
2015-04-29 20:06:05 +0800

    审计标准模块

---
+++ Demo/ISDemo/includes/zTreev3.5.12/data/demoData.js
@@ -0,0 +1,32 @@
+var fxNodes =[
+		{id:9, pId:0,name:"风险指标",open:true},
+		{id:1, pId:9, name:"环境风险度",open:true},
+		{id:102, pId:1,name:"机型机队混编/混合机队构型" },
+		{id:103, pId:1,name:"飞机复杂性的影响" },
+		{id:104, pId:1,name:"雇员结构复杂性的影响" },
+		{id:105, pId:1,name:"外包（维修、培训、地服）" },
+		{id:106, pId:1,name:"季节性运行" },
+		{id:107, pId:1,name:"设施的增添"} ,
+		{id:108, pId:1,name:"搬迁或关闭" },
+		{id:109, pId:1,name:"租赁协定" },
+		{id:110, pId:1,name:"局方正常办公时间以外的航空承运人活动（非办公时间活动）"} ,
+			
+		{id:2, pId:9, name:"运行稳定性",open:true},
+		{id:201, pId:2,name:"高层管理人员的改变" },
+		{id:202, pId:2,name:"其它管理人员的改变" },
+		{id:203, pId:2,name:"人员流动" },
+		{id:204, pId:2,name:"缩减员工" },
+		{id:205, pId:2,name:"运营规模的重大变化" },
+		{id:206, pId:2,name:"兼并或接管"},
+		{id:207, pId:2,name:"劳资关系"} ,				   
+			
+		{id:3, pId:9, name:"绩效历史表现",open:true},
+		{id:301, pId:3,name:"整改措施的执行" },
+		{id:302, pId:3,name:"事故/事故征候/不安全事件" },
+		{id:303, pId:3,name:"航空承运人的信息"},		   
+			
+		{id:4, pId:9, name:"航空承运人动态性",open:true},
+		{id:401, pId:4,name:"大纲的重大改变" },
+		{id:402, pId:4,name:"安全管理体系" },
+		{id:403, pId:4,name:"与局方的合作关系"} 			   
+];
